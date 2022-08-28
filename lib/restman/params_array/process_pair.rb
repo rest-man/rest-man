8a16e74a7ff7784c5cca7ecc81d128d16c35b4e6
Hopper Gee <hopper.gee@hey.com>
2022-08-28 18:15:19 +0800

    Refactor ProcessPair

--- lib/restman/params_array/process_pair.rb
+++ lib/restman/params_array/process_pair.rb
@@ -9,19 +9,29 @@ module RestMan
       def call
         case pair
         when Hash
-          if pair.length != 1
-            raise ArgumentError.new("Bad # of fields for pair: #{pair.inspect}")
-          end
-          pair.to_a.fetch(0)
+          convert_hash_pair_to_array
         when Array
-          if pair.length > 2
-            raise ArgumentError.new("Bad # of fields for pair: #{pair.inspect}")
-          end
-          [pair.fetch(0), pair[1]]
+          parse_array_pair
         else
-          # recurse, converting any non-array to an array
           ProcessPair.call(pair.to_a)
         end
+      end
+
+      private
+
+      def convert_hash_pair_to_array
+        if pair.length != 1
+          raise ArgumentError.new("Bad # of fields for pair: #{pair.inspect}")
+        end
+
+        pair.to_a.fetch(0)
+      end
+
+      def parse_array_pair
+        if pair.length > 2
+          raise ArgumentError.new("Bad # of fields for pair: #{pair.inspect}")
+        end
+        [pair.fetch(0), pair[1]]
       end
 
     end
