3dfc19b0e2e107d4a828e8d73cc14d3b09a93010
Hopper Gee <hopper.gee@hey.com>
2022-09-15 20:20:15 +0800

    Move Payload::Streamed tests to a seperate file

--- spec/unit/payload_spec.rb
+++ spec/unit/payload_spec.rb
@@ -72,41 +72,6 @@ describe RestMan::Payload, :include_helpers do
 
   end
 
-  context "streamed payloads" do
-    it "should properly determine the size of file payloads" do
-      f = File.new(test_image_path)
-      payload = RestMan::Payload.generate(f)
-      expect(payload.size).to eq 72_463
-      expect(payload.length).to eq 72_463
-    end
-
-    it "should properly determine the size of other kinds of streaming payloads" do
-      s = StringIO.new 'foo'
-      payload = RestMan::Payload.generate(s)
-      expect(payload.size).to eq 3
-      expect(payload.length).to eq 3
-
-      begin
-        f = Tempfile.new "rest-man"
-        f.write 'foo bar'
-
-        payload = RestMan::Payload.generate(f)
-        expect(payload.size).to eq 7
-        expect(payload.length).to eq 7
-      ensure
-        f.close
-      end
-    end
-
-    it "should have a closed? method" do
-      f = File.new(test_image_path)
-      payload = RestMan::Payload.generate(f)
-      expect(payload.closed?).to be_falsey
-      payload.close
-      expect(payload.closed?).to be_truthy
-    end
-  end
-
   context "Payload generation" do
     it "should recognize standard urlencoded params" do
       expect(RestMan::Payload.generate({"foo" => 'bar'})).to be_kind_of(RestMan::Payload::UrlEncoded)
