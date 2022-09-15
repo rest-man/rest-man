af8d20c57dc40999d5009d9a6828caf26af43928
Hopper Gee <hopper.gee@hey.com>
2022-09-15 23:07:14 +0800

    Move Request#normalize_url's tests to a seperate file

--- spec/unit/request_spec.rb
+++ spec/unit/request_spec.rb
@@ -47,37 +47,6 @@ describe RestMan::Request, :include_helpers do
     end
   end
 
-  describe '.normalize_url' do
-    it "adds http:// to the front of resources specified in the syntax example.com/resource" do
-      expect(@request.normalize_url('example.com/resource')).to eq 'http://example.com/resource'
-    end
-
-    it 'adds http:// to resources containing a colon' do
-      expect(@request.normalize_url('example.com:1234')).to eq 'http://example.com:1234'
-    end
-
-    it 'does not add http:// to the front of https resources' do
-      expect(@request.normalize_url('https://example.com/resource')).to eq 'https://example.com/resource'
-    end
-
-    it 'does not add http:// to the front of capital HTTP resources' do
-      expect(@request.normalize_url('HTTP://example.com/resource')).to eq 'HTTP://example.com/resource'
-    end
-
-    it 'does not add http:// to the front of capital HTTPS resources' do
-      expect(@request.normalize_url('HTTPS://example.com/resource')).to eq 'HTTPS://example.com/resource'
-    end
-
-    it 'raises with invalid URI' do
-      expect {
-        RestMan::Request.new(method: :get, url: 'http://a@b:c')
-      }.to raise_error(URI::InvalidURIError)
-      expect {
-        RestMan::Request.new(method: :get, url: 'http://::')
-      }.to raise_error(URI::InvalidURIError)
-    end
-  end
-
   describe "user - password" do
     it "extracts the username and password when parsing http://user:password@example.com/" do
       @request.send(:parse_url_with_auth!, 'http://joe:pass1@example.com/resource')
