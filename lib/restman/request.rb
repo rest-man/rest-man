9deaf22902f4c64defc37f1e4afd6cb7c1c766aa
Hopper Gee <hopper.gee@hey.com>
2022-11-17 23:27:20 +0800

    Refactor Request#stringify_headers to StringifyHeaders

--- lib/restman/request.rb
+++ lib/restman/request.rb
@@ -7,6 +7,7 @@ module RestMan
   class Request
 
     autoload :MaybeConvertExtension, 'restman/request/maybe_convert_extension'
+    autoload :StringifyHeaders, 'restman/request/stringify_headers'
 
     include ActiveMethod
     include Init
@@ -233,29 +234,7 @@ module RestMan
     end
 
     # :include: _doc/lib/restman/request/stringify_headers.rdoc
-    def stringify_headers headers
-      headers.inject({}) do |result, (key, value)|
-        if key.is_a? Symbol
-          key = key.to_s.split(/_/).map(&:capitalize).join('-')
-        end
-        if 'CONTENT-TYPE' == key.upcase
-          result[key] = maybe_convert_extension(value.to_s)
-        elsif 'ACCEPT' == key.upcase
-          # Accept can be composed of several comma-separated values
-          if value.is_a? Array
-            target_values = value
-          else
-            target_values = value.to_s.split ','
-          end
-          result[key] = target_values.map { |ext|
-            maybe_convert_extension(ext.to_s.strip)
-          }.join(', ')
-        else
-          result[key] = value.to_s
-        end
-        result
-      end
-    end
+    active_method :stringify_headers
 
     # :include: _doc/lib/restman/request/default_headers.rdoc
     def default_headers
